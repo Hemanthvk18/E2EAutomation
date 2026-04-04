201656d7a10956b213eb66bb5a49f0757f331781
hemanth <hemanthvk2718@gmail.com>
2026-04-04 12:38:13 +0530

    Fix GitHub Actions workflows: Add conditional rerun, fix env vars, update surefire config, remove BeforeSuite race condition

--- src/test/java/testrunner/ParallelTestNgTestRunner.java
+++ src/test/java/testrunner/ParallelTestNgTestRunner.java
@@ -1,21 +1,19 @@
 package testrunner;
 
-//@RunWith(Cucumber.class)
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
-import utilities.FileUtility;
 
 @Listeners({org.testng.reporters.FailedReporter.class})
 @CucumberOptions(features = "src/test/java/features/",
         glue = {"stepdefinitions", "Hook"},
         monochrome = true,
         plugin = {"pretty",
-        "html:target/cucumber-reports/cucumber-pretty", "json:target/cucumber-reports/cucumber.json",
-        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "rerun:target/rerun.txt"},
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/rerun.txt"},
         tags = "@Regression and not @NA")
 
 public class ParallelTestNgTestRunner extends AbstractTestNGCucumberTests {
@@ -25,9 +23,5 @@ public class ParallelTestNgTestRunner extends AbstractTestNGCucumberTests {
     public Object[][] scenarios() {
         return super.scenarios();
     }
-    @BeforeSuite
-    public void beforeSuite() throws Exception {
-        FileUtility.cleanUpFolder("allure-results");
-    }
-    }
+}
 
