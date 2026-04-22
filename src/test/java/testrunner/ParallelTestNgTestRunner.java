b11beabc885141b297305d7fdd6a8767f6a55122
hemanth <hemanthvk2718@gmail.com>
2026-04-22 22:04:18 +0530

    New Update 4

--- src/test/java/testrunner/ParallelTestNgTestRunner.java
+++ src/test/java/testrunner/ParallelTestNgTestRunner.java
@@ -1,9 +1,13 @@
 package testrunner;
 
+//@RunWith(Cucumber.class)
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
+import utilities.FileUtility;
 
 @Listeners({org.testng.reporters.FailedReporter.class})
 @CucumberOptions(features = "src/test/java/features/",
@@ -14,7 +18,7 @@ import org.testng.annotations.Listeners;
                 "json:target/cucumber-reports/cucumber.json",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/rerun.txt"},
-        tags = "@Regression and not @NA")
+        tags = "@SearchProduct")
 
 public class ParallelTestNgTestRunner extends AbstractTestNGCucumberTests {
 
@@ -22,6 +26,11 @@ public class ParallelTestNgTestRunner extends AbstractTestNGCucumberTests {
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
+    }
+
+    @BeforeSuite
+    public void beforeSuite() throws Exception {
+        FileUtility.cleanUpFolder("allure-results");
     }
 }
 
--- src/test/java/testrunner/RerunFailedTestNgRunner.java
+++ src/test/java/testrunner/RerunFailedTestNgRunner.java
@@ -1,19 +1,18 @@
 package testrunner;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "@target/rerun.txt", // <- only failed scenarios from the first run
-        glue = {"com.etool.automation.stepdefinitions", "Hook"},
+        glue = {"stepdefinitions", "Hook"},
         monochrome = true,
-        plugin = {
-                "pretty",
-                "html:target/cucumber-reports/rerun-pretty",
-                "json:target/cucumber-reports/rerun.json",
+        plugin = {"pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/cucumber.json",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-                "rerun:target/rerun.txt"
-        }
+                "rerun:target/rerun.txt"}
 )
 
 public class RerunFailedTestNgRunner extends AbstractTestNGCucumberTests {
